eb4ca7cd23b28d2d4779c2ef279419a53dc3f98b
iago ambrosio <iago_ambrosio@outlook.com>
2022-07-05 15:43:08 -0300

    Corrigindo e colocando uma senha para o usuário wordpress

--- docker/Dockerfile.php
+++ docker/Dockerfile.php
@@ -1,1 +1,12 @@
-FROM 
+FROM ubuntu:jammy
+
+RUN apt update && apt insall \
+    php-fpm:7.4-fpm -y
+
+RUN groupadd wordpress && useradd -ms /bin/bash -g wordpress wordpress
+
+RUN mkdir -p /home/wordpress/.ssh
+
+COPY ./sftp/id_rsa /home/wordpress/.ssh/id_rsa
+
+RUN chown wordpress:wordpress /home/wordpress/.ssh/authorized_keys && chmod 600 /home/wordpress/.ssh/authorized_keys 
