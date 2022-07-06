e3e1770d8b8e83a6307b063eb6e52c357133942e
iago ambrosio <iago_ambrosio@outlook.com>
2022-07-05 22:02:01 -0300

    Dockerfile do php configurada

--- docker/Dockerfile.php
+++ docker/Dockerfile.php
@@ -1,12 +1,24 @@
 FROM ubuntu:jammy
 
-RUN apt update && apt insall \
-    php-fpm:7.4-fpm -y
+#enviroment variables for Debian Frontend
+ENV DEBIAN_FRONTEND=noninteractive 
+#enviorment variable for tzdata
+ENV TZ=Etc/UTC
+#install php-fpm8.1
+RUN apt update && apt install php-fpm=2:8.1+92ubuntu1 -y
 
+#copia arquivos de configuração
+COPY ./docker/php/php.ini /etc/php/8.1/fpm/php.ini
+COPY ./docker/php/php-fpm.conf /etc/php/8.1/fpm/php-fpm.conf
+COPY ./docker/php/pool.d/www.conf /etc/php/8.1/fpm/pool.d/www.conf
+
+# Cria grupos wordpress e usuario wordpress
 RUN groupadd wordpress && useradd -ms /bin/bash -g wordpress wordpress
-
+#create wordpress directory in home
 RUN mkdir -p /home/wordpress/.ssh
-
-COPY ./sftp/id_rsa /home/wordpress/.ssh/id_rsa
-
+#copia a chave privada para acessar o container sftp
+COPY ./docker/sftp/id_rsa /home/wordpress/.ssh/authorized_keys
+#change ownership of the key file.
 RUN chown wordpress:wordpress /home/wordpress/.ssh/authorized_keys && chmod 600 /home/wordpress/.ssh/authorized_keys 
+
+CMD php-fpm8.1 -F
